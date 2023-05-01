01399f83d3cc38c7e2084c5f971adf0522a6cc5f
Sayek17 <isasayek@gmail.com>
2023-05-01 15:35:57 +0600

    commit

--- model/mongodb.js
+++ model/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/test").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/housing_portal").then(()=>{
     console.log('mongoose connected');
 })
 .catch((e)=>{
